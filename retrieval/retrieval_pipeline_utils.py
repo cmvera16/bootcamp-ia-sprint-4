20277429b896c062f7a8c4d31b61ac2a7b2cbcbd
cmvera16 <cynthia.merovera@gmail.com>
2025-08-31 00:58:11 +0200

    Update retrieval_pipeline_utils.py

--- retrieval/retrieval_pipeline_utils.py
+++ retrieval/retrieval_pipeline_utils.py
@@ -1,3 +1,11 @@
+# retrieval/retrieval_pipeline_utils.py
+
+import re
 def clean_query_txt(query: str) -> str:
-    query = query.replace("El usuario busca ", "").strip()
+  """
+    Esta función toma la query de entrada y la limpia para su posterior procesamiento.
+    Puede incluir eliminación de caracteres especiales, conversión a minúsculas, etc.
+    """
+    query = query.lower()  # Convertir a minúsculas
+    query = re.sub(r'[^a-zA-Z0-9\s]', '', query)  # Eliminar caracteres no alfanuméricos
     return query
